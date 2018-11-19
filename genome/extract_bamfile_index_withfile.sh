444bcf17a4c2ebfde13a60b4e338633ce346b8ba
ming wen <35635186+bioming@users.noreply.github.com>
2018-11-19 12:24:00 +0100

    Update extract_bamfile_index_withfile.sh

--- genome/extract_bamfile_index_withfile.sh
+++ genome/extract_bamfile_index_withfile.sh
@@ -10,3 +10,6 @@ export PATH=/usr/local/bioinfo/bin/samtools/samtools-1.1:$PATH
 
 samtools view -h -b L_goldfish_poolfemale_merged.bam flattened_line_58930 > flattened_line_58930_1.bam
 samtools index flattened_line_58930_1.bam
+
+#extract region bam
+samtools view -hb input.bam "Chr10:18000-45500" > output.bam
