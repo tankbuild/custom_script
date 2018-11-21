4fd66c90128a6a5942a2480e96fabb72840dc197
ming wen <35635186+bioming@users.noreply.github.com>
2018-11-21 14:14:51 +0100

    Update poolsex.sh

--- poolsex_analysis/poolsex.sh
+++ poolsex_analysis/poolsex.sh
@@ -27,7 +27,7 @@ then
 fi
 
 # path to PoolSex to generate shell files for each part of the pipeline and submit them on an SGE or a SLURM scheduler
-poolsex.py=/home/mwen/work/tools/PoolSex/poolsex.py
+poolsex=/home/mwen/work/tools/PoolSex/poolsex.py
 
 # path to PoolSex_analysis
 poolsex_analysis=/home/mwen/work/tools/PoolSex-analyses/bin/poolsex
@@ -38,20 +38,21 @@ rootdir=/home/mwen/work/goldfish_poolsex_analysis/poolseq
 #check genome file
 if [ ! -f $rootdir/genome/*.fasta ]
 then
-    ln -s /home/mwen/work/genomes/goldfish/female/GCF_003368295.1_ASM336829v1_genomic.fna goldfish_genome.fasta
+    ln -s /home/mwen/work/genomes/goldfish/female/GCF_003368295.1_ASM336829v1_genomic.fna $rootdir/genome/goldfish_genome.fasta
 fi
 
 # check reads is ready to use
 if [ ! -f $rootdir/reads/*.gz ]
 then
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_femelle_S2_L001_R1_001.fastq.gz female_L1_R1.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_femelle_S2_L001_R2_001.fastq.gz female_L1_R2.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_femelle_S2_L002_R1_001.fastq.gz female_L2_R1.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_femelle_S2_L002_R2_001.fastq.gz female_L2_R2.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_male_S1_L001_R1_001.fastq.gz male_L1_R1.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_male_S1_L001_R2_001.fastq.gz male_L1_R2.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_male_S1_L002_R1_001.fastq.gz male_L2_R1.fasta.gz
-    ln -s /home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData/C_auratus_male_S1_L002_R2_001.fastq.gz male_L2_R2.fasta.gz
+    readsdir=/home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData
+    ln -s $readsdir/C_auratus_femelle_S2_L001_R1_001.fastq.gz $rootdir/reads/female_L1_R1.fasta.gz
+    ln -s $readsdir/C_auratus_femelle_S2_L001_R2_001.fastq.gz $rootdir/reads/female_L1_R2.fasta.gz
+    ln -s $readsdir/C_auratus_femelle_S2_L002_R1_001.fastq.gz $rootdir/reads/female_L2_R1.fasta.gz
+    ln -s $readsdir/C_auratus_femelle_S2_L002_R2_001.fastq.gz $rootdir/reads/female_L2_R2.fasta.gz
+    ln -s $readsdir/C_auratus_male_S1_L001_R1_001.fastq.gz $rootdir/reads/male_L1_R1.fasta.gz
+    ln -s $readsdir/C_auratus_male_S1_L001_R2_001.fastq.gz $rootdir/reads/male_L1_R2.fasta.gz
+    ln -s $readsdir/C_auratus_male_S1_L002_R1_001.fastq.gz $rootdir/reads/male_L2_R1.fasta.gz
+    ln -s $readsdir/C_auratus_male_S1_L002_R2_001.fastq.gz $rootdir/reads/male_L2_R2.fasta.gz
 fi
 
 # path to reference
@@ -71,9 +72,9 @@ male_L2_R1=$rootdir/reads/male_L2_R1.fasta.gz
 male_L2_R2=$rootdir/reads/male_L2_R2.fasta.gz
 
 # run PoolSex
-python $poolsex.py init -i $rootdir &&
+python $poolsex init -i $rootdir &&
 
-python $poolsex.py run -i $rootdir &&
+python $poolsex run -i $rootdir &&
 
 # run PoolSex-analyses
 # generate output data with prefix goldfish_<freq-het>_<freq-hom>_<range-het>_<range-hom>_pool
@@ -82,22 +83,3 @@ $poolsex_analysis --input-file $rootdir/results/mpileup2sync_female_male.sync --
 
 # # clean data
 # python $poolsex.py clean -i $rootdir
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
