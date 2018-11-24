ff1f934d73db61d1502d48965b4a21726b686ea5
ming wen <35635186+bioming@users.noreply.github.com>
2018-11-24 13:20:25 +0100

    Update seq_length.py

--- seq_length.py
+++ seq_length.py
@@ -6,4 +6,4 @@ import sys
 cmdargs = str(sys.argv)
 for seq_record in SeqIO.parse(str(sys.argv[1]),"fasta"):
     output_line = '%s\t%i' % (seq_record.id, len(seq_record))
-    print output_line
+    print(output_line)
