57565fed76fc712ecb24c393a23cd6aaed1f43d5
ming wen <35635186+bioming@users.noreply.github.com>
2019-04-02 15:28:30 +0200

    Add files via upload

---
+++ genome/remove_small_sequence_infasta.pl
@@ -0,0 +1,20 @@
+## removesmalls.pl
+#!/usr/bin/perl
+# perl removesmalls.pl 200 contigs.fasta > contigs-l200.fasta
+use strict;
+use warnings;
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen);
+    }
+    local $/="\n";
+}
