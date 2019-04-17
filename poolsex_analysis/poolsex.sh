5509d4e9baf841c47c47944b4f87aeba45f36fb9
ming wen <35635186+bioming@users.noreply.github.com>
2019-04-17 12:01:29 +0200

    Update poolsex.sh

--- poolsex_analysis/poolsex.sh
+++ poolsex_analysis/poolsex.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/sh
-#SBATCH -J male_SVs_calling
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
-#SBATCH -o /home/mwen/work/goldfish_poolsex_analysis/poolsex.output
-#SBATCH -e /home/mwen/work/goldfish_poolsex_analysis/poolsex.error
+#!/bin/sh
+#BATCH -J unmapped_reads_poolseq
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH -o /home/mwen/work/Scyliorhinus_canicula/poolsex.output
+#SBATCH -e /home/mwen/work/Scyliorhinus_canicula/poolsex.error
 
 module load system/Python-3.6.3
 
@@ -12,54 +12,67 @@ module load system/Python-3.6.3
 # when you switch to another species, just modify rootdir, genome, reads, gffand also reference path
 # sbatch poolsex.sh
 
-# software required and installation
-# 1 PoolSex
-if [ ! -d /home/mwen/work/tools/PoolSex ]
+# prepare folder for poosex analysis in current directory
+if [ ! -d ./poolseq ]
 then
+    mkdir -p poolseq/genome
+    mkdir -p poolseq/reads
+fi
+
+#root directory for poolsex analysis
+rootdir=$(pwd)/poolseq
+
+# software required and installation
+# path to directory for installing tools
+toolsdir=/home/mwen/work/tools
+
+# 1 PoolSex
+if [ ! -d $toolsdir/PoolSex ]
+then
+    cd $toolsdir  # to the directory to install PoolSex tools
     git clone git@github.com:tankbuild/PoolSex.git
 fi
 
 # 2 PoolSex-analysis
-if [ ! -d /home/mwen/work/tools/PoolSex-analyses ]
+if [ ! -d $toolsdir/PSASS ]
 then
-    git clone git@github.com:INRA-LPGP/PoolSex-analyses.git
+    cd "$toolsdir"  # to the directory to install PoolSex-analysis tools
+    git clone https://github.com/RomainFeron/PSASS.git
+    cd ./PSASS
+    module load compiler/gcc-7.2.0
     make
 fi
 
+#bach to working directory
+cd "$rootdir"  #back to the root directory to run script
+
 # path to PoolSex to generate shell files for each part of the pipeline and submit them on an SGE or a SLURM scheduler
-poolsex=/home/mwen/work/tools/PoolSex/poolsex.py
+poolsex=$toolsdir/PoolSex/poolsex.py
 
 # path to PoolSex_analysis
-psass=/home/mwen/work/tools/PSASS/bin/psass
-
-#root directory for poolsex analysis
-rootdir=/home/mwen/work/goldfish_poolsex_analysis/poolseq
+psass=$toolsdir/PSASS/bin/psass
 
 #check genome file
 if [ ! -f $rootdir/genome/*.fasta ]
 then
-    ln -s /home/mwen/work/genomes/goldfish/female/GCF_003368295.1_ASM336829v1_genomic.fna $rootdir/genome/goldfish_genome.fasta
+    ln -s /home/mwen/work/Scyliorhinus_canicula/male_unmapped_reads_reassembly/male_unmapped/scaffolds.fasta $rootdir/genome/dogfish_genome.fasta
 fi
 
 # check reads is ready to use
 if [ ! -f $rootdir/reads/*.gz ]
 then
-    readsdir=/home/mwen/work/Project_PhyloSex.280/Run_A_melas_-_C_auratus.10511/RawData
-    ln -s $readsdir/C_auratus_femelle_S2_L001_R1_001.fastq.gz $rootdir/reads/female_L001_R1.fastq.gz
-    ln -s $readsdir/C_auratus_femelle_S2_L001_R2_001.fastq.gz $rootdir/reads/female_L001_R2.fastq.gz
-    ln -s $readsdir/C_auratus_femelle_S2_L002_R1_001.fastq.gz $rootdir/reads/female_L002_R1.fastq.gz
-    ln -s $readsdir/C_auratus_femelle_S2_L002_R2_001.fastq.gz $rootdir/reads/female_L002_R2.fastq.gz
-    ln -s $readsdir/C_auratus_male_S1_L001_R1_001.fastq.gz $rootdir/reads/male_L001_R1.fastq.gz
-    ln -s $readsdir/C_auratus_male_S1_L001_R2_001.fastq.gz $rootdir/reads/male_L001_R2.fastq.gz
-    ln -s $readsdir/C_auratus_male_S1_L002_R1_001.fastq.gz $rootdir/reads/male_L002_R1.fastq.gz
-    ln -s $readsdir/C_auratus_male_S1_L002_R2_001.fastq.gz $rootdir/reads/male_L002_R2.fastq.gz
+    readsdir=/home/mwen/work/Project_PhyloSex.280/Run_pool-14poissons-Run3.15030/RawData
+    ln -s $readsdir/S-cani-F_GATCTATC-ATGAGGCT-BHGWY2DSXX_L001_R1.fastq.gz $rootdir/reads/female_L001_R1.fastq.gz
+    ln -s $readsdir/S-cani-F_GATCTATC-ATGAGGCT-BHGWY2DSXX_L001_R2.fastq.gz $rootdir/reads/female_L001_R2.fastq.gz
+    ln -s $readsdir/S-cani-M_CATAATAC-TTCTAACG-BHGWY2DSXX_L001_R1.fastq.gz $rootdir/reads/male_L001_R1.fastq.gz
+    ln -s $readsdir/S-cani-M_CATAATAC-TTCTAACG-BHGWY2DSXX_L001_R2.fastq.gz $rootdir/reads/male_L001_R2.fastq.gz
 fi
 
 # path to reference
-reference=$rootdir/genome/goldfish_genome.fasta
+reference=$rootdir/genome/dogfish_genome.fasta
 
 # path to gff file
-gff=/home/mwen/work/genomes/goldfish/female/GCF_003368295.1_ASM336829v1_genomic.gff
+# gff=/home/mwen/work/genomes/goldfish/female/GCF_003368295.1_ASM336829v1_genomic.gff
 
 # run PoolSex
 python $poolsex init -i $rootdir &&
@@ -71,8 +84,8 @@ python $poolsex run -i $rootdir &&
 
 # run PoolSex-analyses
 # generate output data with prefix goldfish_<freq-het>_<freq-hom>_<range-het>_<range-hom>_pool
-$psass analyze --input-file $rootdir/results/mpileup2sync_female_male.sync --output-prefix $rootdir/results/goldfish_0.5_1_0.2_0.05_pool \
-                  --gff-file $gff --male-pool 2 --range-het 0.1 --range-hom 0.05
-
-# # clean data
-# python $poolsex.py clean -i $rootdir
+until [ -f $rootdir/results/mpileup2sync_female_male.sync ]
+do
+	$psass analyze --input-file $rootdir/results/mpileup2sync_female_male.sync --output-prefix $rootdir/results/dogfish_0.5_1_0.1_0.05_pool \
+	               --male-pool 2 --range-het 0.1 --range-hom 0.05
+done
