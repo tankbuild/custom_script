e44007de22d41f218ccf5080d1331d4ef392f945
ming wen <35635186+bioming@users.noreply.github.com>
2020-07-15 14:47:59 +0800

    Add files via upload

---
+++ gbff2gff3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -J gbff2gff3
+#SBATCH --cpus-per-task=4
+#SBATCH -o /home/aherpin/work/bsb/gbff2gff.output
+#SBATCH -e /home/aherpin/work/bsb/gbff2gff.error
+
+module purge
+
+perl ~/work/tools/BioPerl-1.7.7/bin/bp_genbank2gff3 bsb.gbff.gz > bsb.gff3
